c70f22378780fd1ed46c86110b286a6e13ed17cb
BethMoote <beth.moote@gmail.com>
2016-03-08 10:18:56 -0500

    Trying to implement a slash command for russian roulette

--- index.js
+++ index.js
@@ -4,6 +4,17 @@ var express = require('express');
 var app = express();
 var url = require('url');
 
+var express        =        require("express");
+var bodyParser     =        require("body-parser");
+var app            =        express();
+
+var ACCESS_TOKEN = "xoxp-24999931810-24999636768-25360127220-4879e4d896";
+var BOT_ACCESS_TOKEN = "xoxb-25049965060-BXU8Ua6XPvBIgyD4MbuQLmtc";
+
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
@@ -71,4 +82,47 @@ console.log("Skipping ", property);
    res.json(properties);
 });
 
+
+app.post('/roulette',function(req,res){
+  var request=JSON.stringify(req.body);
+  console.log("request = "+request);
+
+ var request = require('request');
+// var propertiesObject = { token:'xoxb-25049965060-ki7nRZf2ROLLT4zOqo9qwM0U', channel:req.body.channel_id };
+
+// request({url:"https://slack.com/api/channels.info", qs:propertiesObject}, function(err, response, body) {
+//  if(err) { console.log(err); return; }
+//  
+//  console.log("/roulette text: " + req.body.text)
+//  console.log("Get response: " + response);
+//  console.log("Get body: " + body);
+//  var bodyJson = JSON.parse(body);
+//  console.log("Get Channel:" + bodyJson.channel);
+//  console.log("Get Members: " + bodyJson.channel.members);
+//  var memberArray = bodyJson.channel.members
+// 
+//  var rand = memberArray[Math.floor(Math.random() * memberArray.length)];
+//  console.log("random Member id: ", rand);
+ 	
+   var userGroupObject = { token:ACCESS_TOKEN };
+   request({url:"https://slack.com/api/usergroups.list", qs:userGroupObject}, function(err, response, body) {
+      var convertedBody = JSON.parse(body);
+      console.log("converted body: " + JSON.stringify(convertedBody));
+      console.log("usergroups: " + JSON.stringify(convertedBody["usergroups"]));
+      var usergroups = convertedBody["usergroups"];
+      for (var i = 0; i < usergroups.length; i++) {
+         var group = usergroups[i];
+console.log("Comparing " + req.body.text + " to " + group["handle"]);
+         if (req.body.text == group.handle) {
+            console.log("The magic ID is: " + group.id);
+         } else {
+            console.log("You suck!");
+         } 
+      }
+   })
+ 
+//}) 
+
+  res.end("yes");
+});
 
