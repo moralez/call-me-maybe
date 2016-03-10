f106ab05544b255be5b4e37270485bc291632b3a
BethMoote <beth.moote@gmail.com>
2016-03-10 11:46:09 -0500

    redis implementation

--- index.js
+++ index.js
@@ -185,6 +185,21 @@ app.post('/checkins', function(req, res) {
    // });
 });
 
+app.post('/ag', function(req, res){
+//given zip code, parse out zip code from request
+var request = JSON.parse(req.body);
+console.log("Request: " + JSON.stringify(request));
+
+var zipCode = request.query
+console.log("Query: " + zipCode);
+
+//send zip code to http://m.api.qa.apartmentguide.com/search?query=30092
+//parse out first 10 listings
+//parse out each of listings' city, state, bhs, beds, adr, photo, prices, name
+
+
+});
+
 app.post('/roulette',function(req,res){
    var request=JSON.stringify(req.body);
    console.log("request = "+request);
