bcb3cbc00c06586e29c70a26cbcebb49ed6d7863
Johnny Moralez <moralez@gmail.com>
2016-03-11 15:00:34 -0500

    [BM/JMO] Adding Reactions

--- index.js
+++ index.js
@@ -269,7 +269,7 @@ app.post('/checkins', function(req, res) {
 
                var today8AM = new Date();
                console.log(today8AM)
-               today8AM.setHours(8);
+               today8AM.setHours(13);
                console.log(today8AM)
                today8AM.setMinutes(0);
                console.log(today8AM)
