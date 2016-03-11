346329c1c3e96fd09b485f867f3f02569a7895ca
BethMoote <beth.moote@gmail.com>
2016-03-11 15:24:54 -0500

    adding next page functionality to AG

--- index.js
+++ index.js
@@ -300,7 +300,7 @@ app.post('/checkins', function(req, res) {
 
                       var userInfo = { token:ACCESS_TOKEN, user:userID };
                       requestHelper({url:"https://slack.com/api/users.info", qs:userInfo}, function(err, response, body) {
-                        console.log("Response: " + response);
+                        console.log("Response: " + JSON.stringify(response));
                         console.log("Body: " + body);
                         var parsedBody = JSON.parse(body);
                         var name = parsedBody.first_name + " " + parsedBody.last_name;
