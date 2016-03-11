701ba477490ddf28fd69410f92a63d02ba6d6b8f
BethMoote <beth.moote@gmail.com>
2016-03-11 10:34:22 -0500

    adding next page functionality to AG

--- index.js
+++ index.js
@@ -327,7 +327,8 @@ function parseThroughListings(id, zipCode, page) {
       console.log("Message Text: " + messageText);
     }
     
-    messageText = messageText + "\n\n Please select from options 0 - " + (listings.length - 1) + " for Contact Information on that property";
+    messageText = messageText + "\n\n Please select from options 0 - " + (listings.length - 1) + " for Contact Information on that property.\n\n
+    Please add the reaction :right_arrow: to go the next page of results."
     
     var postMessageParams = { token:BOT_ACCESS_TOKEN, channel:id, text: messageText, as_user: true, parse: "full" };
       request({url:"https://slack.com/api/chat.postMessage", qs:postMessageParams}, function(err, response, body) {
