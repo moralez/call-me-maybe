cd0a3bb654cc9ae515f8e4b9a076f8554d7e2ace
BethMoote <beth.moote@gmail.com>
2016-03-22 09:59:22 -0400

    debugging

--- index.js
+++ index.js
@@ -224,8 +224,8 @@ app.get('/tokens', function(req, res) {
   var tokenString = "";
   var requestBody = req.body;
   console.log("req: " + JSON.stringify(req.body));
+  console.log("req:: " + req);
 
-  
   client.get("ACCESS_TOKEN", function(err, reply) {
     tokenString += reply.toString();
         console.log(res.toString()); // => should be crazy token
